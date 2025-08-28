a66e4bd4f7dd4187f6b11cc87d932592431a0fa2
Mayank Sharma <132637383+MAYANKSHARMA01010@users.noreply.github.com>
2025-08-28 13:27:45 +0530

    Server creatd routed read write and etc

--- module/http/http.js
+++ module/http/http.js
@@ -1,9 +1,39 @@
+// sudo npm install -g nodemon --------> install the nodemon globall and used nodemon http/http.js in package.locker
+// it will auto restart my server whenever we change anything and save the file no need to stop and restart it again and again 
+
 const http = require('http');
+const fs = require('fs');
+const { error } = require('console');
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-    res.end('Hello, How are you?');
+    // it will give the path after 3000
+    console.log(req.url);
+
+    // Log the request method
+    console.log("First request received");
+
+    // Set Header
+    res.writeHead(200, { 'Content-Type': 'Mayank' });
+
+    // Read File and routing
+    if (req.url === "/doggy") {
+        fs.readFile('DOGGY.jpeg',(error,data)=>{
+            res.end(data)
+        })
+    }
+    else if (req.url === '/website') {
+        fs.readFile('index.html',(error,data)=>{
+            res.end(data)
+        })
+    }
+    else {
+        res.end(`Page Not Found`);
+    }
+    
+    // End the Server only one time we can end the server
+    // res.end(`Hello, How are you?`);
 });
 
 server.listen(PORT, () => {
